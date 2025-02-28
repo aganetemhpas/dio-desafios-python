d2b9dd107a01a4774486a8bdcc2d1082def48166
aganetemhpas <matheushenriqueaganete@gmail.com>
2025-02-28 16:36:02 -0300

    POO

---
+++ POO/primeiro_programa_POO.py
@@ -0,0 +1,29 @@
+class Bicicleta:
+    def __init__(self, cor, modelo, ano, valor):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+        self.valor = valor
+
+    def buzinar(self):
+        print("Plim plim")
+    
+    def parar(self):
+        print("Parando bicicleta...")
+        print("Bicicleta parada")
+
+    def correr(self):
+        print("Vrummmm...")
+
+    
+
+b1 = Bicicleta("vermelha", "caloi", 2022, 600)
+b1.buzinar()
+b1.correr()
+b1.parar()
+
+print(b1.cor, b1.modelo, b1.modelo, b1.ano, b1.valor)
+
+b2 = Bicicleta("verde", "monark", 2000, 189)
+b2.buzinar() # Bicicleta.buzinar(b2)
+print(b2.cor)
